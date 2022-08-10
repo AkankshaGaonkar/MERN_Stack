54c35bbf0224c4169ab686b7bda137bd5583a590
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-08-10 11:31:50 +0530

    add

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,3 +1,13 @@
 //jshint version6
-const fs = require("fs");
-fs.copyFileSync("file.txt","file1.txt");
+// const fs = require("fs");
+// fs.copyFileSync("file.txt","file1.txt")
+
+var superheroes = require("superheroes");
+var mysuperheroname = superheroes.random();
+console.log(mysuperheroname);
+
+var supervillains = require("supervillains")
+var villain = supervillains.random();
+console.log(villain);
+
+
