7cd04d782c7ec136a3e0ab0c517021e83552f65d
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-08-10 12:53:04 +0530

    add

--- jQuerry/index.js
+++ jQuerry/index.js
@@ -1,1 +1,12 @@
 $("h1").css("color","red");
+
+for(var i=0;i<5;i++)
+{
+    document.querySelectorAll("button").addEventListner("click",function(){
+
+    });
+}
+
+$("button").click(function(){
+    $('h1').css("color","purple");
+})
