0d72e841bad879d0431b6f0ca13d852c0a8ee1e6
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-08-15 16:21:14 +0530

    add

--- Express/Calculator/calculator.js
+++ Express/Calculator/calculator.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/",function(req,res){
- res.send("<h1>Hello World</h1>");
-});
-app.get("/aboutme",function(req,res){
-    res.send("Hi!Iam a full stack developer");
+    res.send("<h1>Hello World</h1>");
 });
 app.listen(3000,function(){
     console.log("Server started on port 3000");
